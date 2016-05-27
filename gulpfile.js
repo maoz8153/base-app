f54551291e240730dc0f2ed0584e483f2c76a1fa
maoz8153 <maoz8153@gmail.com>
2016-05-27 04:12:10 +0300

    wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,6 @@
 var gulp = require('gulp');
 var config = require('./gulp-config.js')();
-var $ = require('gulp-load-plugins')({
-    pattern: [
-        'gulp-*', 'gulp.*',
-        'main-bower-files',
-        'uglify-save-license',
-        'del',
-        'merge-stream',
-        'run-sequence',
-        'karma'
-    ]
-});
-
+var $ = require('gulp-load-plugins')();
 var pathScripts =  'public/';
 
 
@@ -25,4 +14,3 @@ gulp.task('inject', function () {
                 .pipe($.inject(gulp.src(config.js), {ignorePath: pathScripts, addRootSlash: false}))
                 .pipe(gulp.dest(config.indexFolder));
 });
-
